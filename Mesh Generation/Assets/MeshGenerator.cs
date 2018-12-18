e819be7ce9ac5699e1828c02a259697f7299d85a
shubham-saudolla <shubhamsaudolla@gmail.com>
2018-12-18 19:20:52 +0530

    Generated a plane and added some perlin noise for terrain generation

--- Mesh Generation/Assets/MeshGenerator.cs
+++ Mesh Generation/Assets/MeshGenerator.cs
@@ -15,6 +15,9 @@ public class MeshGenerator : MonoBehaviour
     Vector3[] vertices;
     int[] triangles;
 
+    public int xSize = 20;
+    public int zSize = 20;
+
     private void Start()
     {
         mesh = new Mesh();
@@ -26,18 +29,40 @@ public class MeshGenerator : MonoBehaviour
 
     void CreateShape()
     {
-        vertices = new Vector3[]
-        {
-            new Vector3 (0,0,0),
-            new Vector3 (0,0,1),
-            new Vector3 (1,0,0),
-            new Vector3 (1,0,1)
-        };
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
 
-        triangles = new int[]
+        for (int i = 0, z = 0; z <= zSize; z++)
         {
-            0, 1, 2, 1, 3, 2
-        };
+            for (int x = 0; x <= xSize; x++)
+            {
+                float y = Mathf.PerlinNoise(x * 0.3f, z * 0.3f) * 2f;
+                vertices[i] = new Vector3(x, y, z);
+                i++;
+            }
+        }
+
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
     }
 
     void UpdateMesh()
@@ -49,4 +74,17 @@ public class MeshGenerator : MonoBehaviour
 
         mesh.RecalculateNormals();
     }
+
+    /*
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+            return;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+    */
 }
